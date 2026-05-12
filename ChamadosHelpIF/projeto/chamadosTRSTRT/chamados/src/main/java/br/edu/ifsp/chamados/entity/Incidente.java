bb2a36833259f5a1bf4c77ecf2c21ec1bd98c907
AndreDev20 <98370867+AndreDev20@users.noreply.github.com>
2026-05-11 23:46:26 -0300

    Update Incidente.java

--- ChamadosHelpIF/projeto/chamadosTRSTRT/chamados/src/main/java/br/edu/ifsp/chamados/entity/Incidente.java
+++ ChamadosHelpIF/projeto/chamadosTRSTRT/chamados/src/main/java/br/edu/ifsp/chamados/entity/Incidente.java
@@ -43,7 +43,7 @@ public class Incidente {
     @Column(nullable = false, columnDefinition = "TEXT")
     private String observacao;
 
-    @Column(columnDefinition = "LONGTEXT")
+    @Column(columnDefinition = "TEXT")
     private String anexo;
 
     @ManyToOne(fetch = FetchType.LAZY)
